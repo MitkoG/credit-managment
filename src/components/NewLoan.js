0d8367f669f9b7d5d759f2470d0cd780aa03b37a
unknown <mitko.genov@gmail.com>
2023-05-20 14:11:45 +0300

    add styles to new loan

--- src/components/NewLoan.js
+++ src/components/NewLoan.js
@@ -18,21 +18,46 @@ function NewLoan({addNewLoan}) {
     setLoanTerm('');
   }
   const styles = {
-    // TODO add styles
+    form: {
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      marginBottom: '20px',
+    },
+    formGroup: {
+      marginBottom: '10px',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
+    input: {
+      padding: '5px',
+      borderRadius: '5px',
+      border: '1px solid #ccc',
+      width: '200px',
+    },
+    button: {
+      padding: '10px 20px',
+      backgroundColor: '#4CAF50',
+      color: 'white',
+      border: 'none',
+      borderRadius: '5px',
+      cursor: 'pointer',
+    },
   };
 
   return (
     <form onSubmit={handleSubmit} style={styles}>
       <h3>New Loan</h3>
-      <div>
+      <div style={styles.formGroup}>
         <label>Name</label>
         <input type='text' value={loanName} onChange={(e) => setLoanName(e.target.value)} required style={styles.input}></input>
       </div>
-      <div>
+      <div style={styles.formGroup}>
         <label>Amount (BGN)</label>
         <input type='number' value={loanAmount} onChange={(e) => setLoanAmount(e.target.value)} required style={styles.input}></input>
       </div>
-      <div>
+      <div style={styles.formGroup}>
         <label>Term (months)</label>
         <input type='number' value={loanTerm} onChange={(e) => setLoanTerm(e.target.value)} min='1' max='12' required style={styles.input}></input>
       </div>
